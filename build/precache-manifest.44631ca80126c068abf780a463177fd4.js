af986cd8cb09a68b1d5dd5f64fc18d4bf77833d1
PeiYumeng <457492875@qq.com>
2019-10-17 21:20:55 +0800

    message

---
+++ build/precache-manifest.44631ca80126c068abf780a463177fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5504b6731698be93ad2f252b3fb222",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97606cb4470804554e64",
+    "url": "/static/css/main.5bca265b.chunk.css"
+  },
+  {
+    "revision": "1153c46624d715ecd183",
+    "url": "/static/js/2.3dd6b646.chunk.js"
+  },
+  {
+    "revision": "97606cb4470804554e64",
+    "url": "/static/js/main.c22cee17.chunk.js"
+  },
+  {
+    "revision": "daa5812f784017d95260",
+    "url": "/static/js/runtime-main.09152487.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
